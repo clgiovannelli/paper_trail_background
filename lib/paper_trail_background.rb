802a18c1eee8820a364c788c1acf6871675f4ed1
Mona Helal <mona.helal@cartona.com>
2021-10-28 16:08:41 +0200

    make version class name variable

--- lib/paper_trail_background.rb
+++ lib/paper_trail_background.rb
@@ -7,4 +7,10 @@ require 'ar_after_transaction'
 require_relative 'paper_trail_background/version'
 require_relative 'paper_trail_background/record_trail'
 
-module PaperTrailBackground; end
+module PaperTrailBackground
+  mattr_accessor :version_job_class
+
+  def self.version_job_class
+    @@version_job_class.constantize
+  end
+end
---
+++ test/dummy/config/initializers/paper_trail_background.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+PaperTrailBackground.version_job_class = 'VersionJob'
